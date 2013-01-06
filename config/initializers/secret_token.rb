7ff8005132fbaf2efe50ee91dcb32237857e85b6
shoumu <shoumuzyq@gmail.com>
2013-01-06 14:08:49 +0800

    first commit, start the work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Talk8::Application.config.secret_token = '6f3aea3051734e87905e49b431f8e70feb009d0630f20614215d388b64435340858696fa476a89bc68795a057a4ce7c5c8cb7899a77487256821cdd89f9e1b9b'
